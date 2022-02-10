df12bf85c2d11a2955b3e1e9cef91a5daaf9ea34
Enjy <enjy.khaled23@gmail.com>
2022-02-10 23:37:15 +0200

    Initial Commit

---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by enjykhaled on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
